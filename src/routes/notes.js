11e7a506862602222f503581f49d840aaa12707f
bugsygel <belmdiez@gmail.com>
2021-10-15 16:10:32 -0300

    unfsosdss

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req,res)=>{
+    res.render('Notas');
+})
+
 router.get('/notes', (req,res)=>{
     res.send('Notas');
 })
