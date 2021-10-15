65c2ac5f1af69abe6b87d8aecf01681fa8420a0d
bugsygel <belmdiez@gmail.com>
2021-10-15 16:27:48 -0300

    unfsosdss

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -6,8 +6,20 @@ router.get('/notes/add', (req,res)=>{
 })
 
 router.post('/notes/new-note', (req,res)=>{
-    console.log(req.body)
-    res.send('ok')
+    const {title}=req.body
+    const errors= [];
+    if(!title){
+        errors.push({text: 'escribir titulo'})
+    }
+    if(errors.length>0){
+        res.render('notes/new-note', {
+            errors, 
+            title
+        });
+    }else{
+        res.send('ok')      
+    }
+    
 })
 
 router.get('/notes', (req,res)=>{
