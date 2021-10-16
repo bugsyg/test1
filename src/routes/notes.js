82bb26a65a9c505a23fcae414dbac773067a961b
bugsygel <belmdiez@gmail.com>
2021-10-16 19:45:02 -0300

    unfsosdss

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -26,7 +26,7 @@ router.post('/notes/new-note', async (req,res)=>{
 })
 
 router.get('/notes', async (req,res)=>{
-    const notes = await Note.find();
+    const notes = await Note.find().lean();
     res.render('notes/all-notes', { notes })
 })
 
