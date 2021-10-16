7128f37e4082317d480092a10a743e7ae9d447f7
bugsygel <belmdiez@gmail.com>
2021-10-16 19:27:20 -0300

    unfsosdss

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Note = require("../models/Note")
+const Note = require("../models/Note") 
 
 router.get('/notes/add', (req,res)=>{
     res.render('notes/new-note');
