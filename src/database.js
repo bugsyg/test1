78a2a7816160d434f165874303df5183f6bfe02f
bugsygel <belmdiez@gmail.com>
2021-10-16 19:19:12 -0300

    unfsosdss

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 var mongoose = require ('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/patolucas', {
+mongoose.connect('mongodb+srv://bugsygel:43442856@cluster0.jijz8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     })
     .then(db => console.log('mongosucksass'))
