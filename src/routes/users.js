1db8912a8134b0f3c67d65b53f8c5ad8390e84e3
bugsygel <belmdiez@gmail.com>
2021-10-17 10:28:04 -0300

    unfsosdss

--- src/routes/users.js
+++ src/routes/users.js
@@ -18,10 +18,10 @@ router.post('/users/signup', (req,res)=>{
     if (password != confirm_password) {
         errors.push({text:'Las Constraseñas no coinciden'})
     }
-    if (password.length > 8 ) {
+    if (password.length < 8 ) {
         errors.push({text:'La contrseña debe tener 8 caracteres'})
     }
-    if (errors.length > 8 ) {
+    if (errors.length > 0 ) {
         res.render('users/signup', {errors, name, email, password, confirm_password})
     } else{
         res.send('ok');
