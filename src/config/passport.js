fed817670cc2688630a2b4bcc15aed028e233b91
bugsygel <belmdiez@gmail.com>
2021-10-17 14:33:06 -0300

    unfsosdss

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,34 +1,39 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 
-const User = require('../models/User');
+import User from "../models/User.js";
 
 passport.use(
-    new LocalStrategy(
-        {
-    usernameField: 'email'},
-    async (email, password, done) =>{
-     const user = await User.findOne({ email: email })
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    async (email, password, done) => {
+      // Match Email's User
+      const user = await User.findOne.catch({ email: email });
 
-     if (!user) {
-         return done(null, false, {message: 'No existe ese usuario'})
-     } else {
-         const match = await user.matchPassword(password);
+      if (!user) {
+        return done(null, false, { message: "Not User found." });
+      } else {
+        // Match Password's User
+        const match = await user.matchPassword(password);
         if (match) {
           return done(null, user);
         } else {
-            return done(null, false, {message: 'La contraseña es errónea'})
-         }
-     }
- }))
+          return done(null, false, { message: "Incorrect Password." });
+        }
+      }
+    }
+  )
+);
 
 
- passport.serializeUser((user, done) => {
-    done(null, user.id);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
   });
-  
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user);
-    });
-  });
+});
