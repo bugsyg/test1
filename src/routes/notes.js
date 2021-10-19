36fe2eeee54a77e5f234ca700fe758b5e8207aaf
bugsygel <belmdiez@gmail.com>
2021-10-19 20:31:28 -0300

    gracias

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -13,36 +13,36 @@ router.post('/notes/new-note', isAuthenticated, async (req,res)=>{
     if (fijo === "No"){
         if(caracter=="Estudio"){
             if(parseInt(duracion, 10)>120 && limite==="No"){
-            duracion="120";
+            duracion=120;
             }
             if(parseInt(cuando, 10) < 1000 || parseInt(cuando, 10) > 2200){
-            cuando="1000";
+            cuando=1000;
             }
             if(parseInt(cuando, 10) > 1400 && parseInt(cuando, 10) < 1600){
-            cuando="1600";
+            cuando=1600;
             }
         
         }
         else if(caracter=="Ejercicio"){
             if(parseInt(duracion, 10)>30 && limite==="No"){
-            duracion="30";
+            duracion=30;
             }
             if(parseInt(cuando, 10) < 1400 || parseInt(cuando, 10) > 1800){
-            cuando="1500";
+            cuando=1500;
             }
         }
         else if(caracter=="Alimentacion"){
             if(parseInt(duracion, 10)<30){
-            duracion="30";
+            duracion=30;
             }
             if(parseInt(cuando, 10) < 930){
-            cuando="930";
+            cuando=930;
             }
             if(parseInt(cuando, 10) > 11 && parseInt(cuando, 10) < 15){
-            cuando="13";
+            cuando=13;
             }
             if(parseInt(cuando, 10) > 1900){
-            cuando="2030";
+            cuando=2030;
             }
     }}
     if(!title){
