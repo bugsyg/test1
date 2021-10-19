ad48834137218bd73afc0689420daed5cc5d120a
bugsygel <belmdiez@gmail.com>
2021-10-19 17:24:40 -0300

    gracias

--- src/models/Note.js
+++ src/models/Note.js
@@ -7,7 +7,7 @@ const NoteSchema = new Schema({
     user: {type: String},
     fijo: {type: String, default: null},
     duracion: {type: Number, default: null},
-    limite: {type: Number, default: null},
+    limite: {type: String, default: null},
     caracter: {type: String, default: null},
     cuando: {type: Number, default: null}
 })
