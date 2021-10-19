f9a5c2a88f06ef31ae66e27202d3e630348b146a
bugsygel <belmdiez@gmail.com>
2021-10-19 20:41:00 -0300

    gracias

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -10,41 +10,7 @@ router.get('/notes/add', isAuthenticated, (req,res)=>{
 router.post('/notes/new-note', isAuthenticated, async (req,res)=>{
     const {title, fijo, cuando, duracion, limite, caracter}=req.body
     const errors= [];
-    if (fijo === "No"){
-        if(caracter=="Estudio"){
-            if(parseInt(duracion, 10)>120 && limite==="No"){
-            duracion=120;
-            }
-            if(parseInt(cuando, 10) < 1000 || parseInt(cuando, 10) > 2200){
-            cuando=1000;
-            }
-            if(parseInt(cuando, 10) > 1400 && parseInt(cuando, 10) < 1600){
-            cuando=1600;
-            }
-        
-        }
-        else if(caracter=="Ejercicio"){
-            if(parseInt(duracion, 10)>30 && limite==="No"){
-            duracion=30;
-            }
-            if(parseInt(cuando, 10) < 1400 || parseInt(cuando, 10) > 1800){
-            cuando=1500;
-            }
-        }
-        else if(caracter=="Alimentacion"){
-            if(parseInt(duracion, 10)<30){
-            duracion=30;
-            }
-            if(parseInt(cuando, 10) < 930){
-            cuando=930;
-            }
-            if(parseInt(cuando, 10) > 11 && parseInt(cuando, 10) < 15){
-            cuando=13;
-            }
-            if(parseInt(cuando, 10) > 1900){
-            cuando=2030;
-            }
-    }}
+    
     if(!title){
         errors.push({text: 'escribir titulo'})
     }
@@ -54,6 +20,41 @@ router.post('/notes/new-note', isAuthenticated, async (req,res)=>{
             title
         });
     }else{
+        if (fijo === "No"){
+            if(caracter=="Estudio"){
+                if(parseInt(duracion, 10)>120 && limite==="No"){
+                duracion=120;
+                }
+                if(parseInt(cuando, 10) < 1000 || parseInt(cuando, 10) > 2200){
+                cuando=1000;
+                }
+                if(parseInt(cuando, 10) > 1400 && parseInt(cuando, 10) < 1600){
+                cuando=1600;
+                }
+            
+            }
+            else if(caracter=="Ejercicio"){
+                if(parseInt(duracion, 10)>30 && limite==="No"){
+                duracion=30;
+                }
+                if(parseInt(cuando, 10) < 1400 || parseInt(cuando, 10) > 1800){
+                cuando=1500;
+                }
+            }
+            else if(caracter=="Alimentacion"){
+                if(parseInt(duracion, 10)<30){
+                duracion=30;
+                }
+                if(parseInt(cuando, 10) < 930){
+                cuando=930;
+                }
+                if(parseInt(cuando, 10) > 11 && parseInt(cuando, 10) < 15){
+                cuando=13;
+                }
+                if(parseInt(cuando, 10) > 1900){
+                cuando=2030;
+                }
+        }}
         const newNote = new Note({title, fijo, cuando, duracion, limite, caracter})
         newNote.user = req.user.id; 
         await newNote.save()
