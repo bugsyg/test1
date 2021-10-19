4533c7835a994217139393aaa7d0238fdcc5d17d
bugsygel <belmdiez@gmail.com>
2021-10-19 16:52:25 -0300

    unfsosdss

--- src/models/Note.js
+++ src/models/Note.js
@@ -4,7 +4,12 @@ const { Schema } = mongoose;
 const NoteSchema = new Schema({
     title: {type: String, required: true},
     date: {type: Date, default: Date.now},
-    user: {type: String}
+    user: {type: String},
+    fijo: {type: String, default: null},
+    duracion: {type: Number, default: null},
+    limite: {type: Date, default: null},
+    caracter: {type: String, default: null},
+    cuando: {type: Date, default: null}
 })
 
 module.exports = mongoose.model('Note', NoteSchema)
