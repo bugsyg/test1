8de1a00d1b58461d3d9db8d0e2c109de20a6e2d5
bugsygel <belmdiez@gmail.com>
2021-11-03 08:37:40 -0300

    gracias

--- src/models/Note.js
+++ src/models/Note.js
@@ -5,10 +5,10 @@ const NoteSchema = new Schema({
     title: {type: String, required: true},
     date: {type: Date, default: Date.now},
     user: {type: String},
-    fijo: {type: String, default: null},
-    duracion: {type: Number, default: null},
+    fijo: {type: String, default: "No"},
+    duracion: {type: String, default: "30 minutos"},
     limite: {type: String, default: null},
-    caracter: {type: String, default: null},
+    caracter: {type: String, default: "Estudio"},
     cuando: {type: String, default: null},
     dia: {type: Date, default: null}
 })
