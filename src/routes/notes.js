99981b9c16d8ec2c45995d928c7f045e2779d517
bugsygel <belmdiez@gmail.com>
2021-11-05 08:38:52 -0300

    gracias

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Note = require("../models/Note") 
 const { isAuthenticated } = require('../helpers/auth')
 const moment= require('moment') 
-const AppointmentPicker = require('appointment-picker');
 
 router.post('/notes/new-note', isAuthenticated, async (req,res)=>{
     const {title, dia, date, fijo, duracion, caracter, cuando}=req.body
@@ -18,8 +17,7 @@ router.post('/notes/new-note', isAuthenticated, async (req,res)=>{
             title
         });
     }else{
-        var picker = new AppointmentPicker(cuando, {})
-        const newNote = new Note({title, dia, date, fijo, duracion, caracter, cuando, picker})
+        const newNote = new Note({title, dia, date, fijo, duracion, caracter, cuando})
         newNote.user = req.user.id; 
         await newNote.save()
         res.redirect('/notes')    
