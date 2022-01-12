c885d1b5fa9af2c1aeca3c52e7935cd994665020
bugsyg <78964358+bugsyg@users.noreply.github.com>
2022-01-12 16:25:07 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 var mongoose = require ('mongoose');
-mongoose.connect('mongodb+srv://bugsygel:43442856@cluster0.jijz8.mongodb.net/cluster0?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://bugsygel:43442856@cluster0-shard-00-00.jijz8.mongodb.net:27017,cluster0-shard-00-01.jijz8.mongodb.net:27017,cluster0-shard-00-02.jijz8.mongodb.net:27017/MyFirstDatabase?ssl=true&replicaSet=atlas-7vyxgv-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     })
     .then(db => console.log('mongosucksass'))
